cee8fa0a8eaa1a2da04195e5df40075f1e906323
prakashprojectslist <prakash.vlsi406@gmail.com>
2026-06-05 17:31:39 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# Create the directory structure for the project
+mkdir -p src
+mkdir -p research
+
+# Creating files for the project
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch  setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory structure  created successfully!"
+
