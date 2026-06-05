897e78efc77f9793fffdf71b39294ba6acc0885b
prakashprojectslist <prakash.vlsi406@gmail.com>
2026-06-05 20:53:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="prakash",
+    author_email="prakash.vlsi406@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
